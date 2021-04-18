37d543a88623e2455bd02fc46dec3d9e43fce3ae
sebastian <sebastian bowen>
2021-04-18 15:14:31 -0600

    have employees adding and can now change roles

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ const initChoices = function () {
                 addRole();
                 break;
             case 'add an employee':
+                addEmployee();
                 break;
             case 'update an employee role':
+                updateEmpRole();
                 break;                      
         }
 
@@ -131,7 +133,76 @@ const viewRoles = () => {
             }
             console.table(result)
         })
+      });
+  };
+
+
+  const addEmployee = () => {
+      inquirer.prompt([
+          {
+              type: 'text',
+              name: 'first_name',
+              message: 'what is employees first name?'
+          },
+          {
+              type: 'text',
+              name: 'last_name',
+              message: 'what is employees last name'
+          },
+          {
+              type: 'integer',
+              name: 'role_id',
+              message: 'what is employees role id?'
+          },
+          {
+              type: 'integer',
+              name: 'manager_id',
+              message: 'what is the employees managers id?'
+          }
+      ]).then(({first_name, last_name, role_id, manager_id}) => {
+        const sql = `INSERT INTO Employee(first_name, last_name, role_id, manager_id)
+        VALUES(?, ?, ?, ?)`
+
+     
+
+        db.query(sql,[first_name, last_name, role_id, manager_id], (err, result) => {
+            if(err) {
+                throw err;
+            }
+           
+                console.table(result);
+            
+            
+        })
+      });
+  };
+
+  const updateEmpRole = () => {
+      viewEmployees();
+      viewRoles();
+      inquirer.prompt([ 
+          { 
+            type: 'decimal',
+            name: 'newRoleId',
+            message: 'what is the employees new role id?'
+          },
+          {
+            type: 'decimal',
+          name: 'id',
+          message: 'what is the id of employee whos role you would like to update? '  
+          }
+      ]).then(({newRoleId, id}) => {
+          const sql = `UPDATE Employee SET role_id = ?
+          WHERE id = ?`;
+          db.query(sql,[newRoleId, id],(err, result) => {
+              if(err) {
+                  throw err;
+              }
+              console.table(result);
+              viewEmployees();
+          })
       })
   }
-
  
+
+  
